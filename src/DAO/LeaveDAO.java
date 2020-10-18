ffe988d055221333889e64c5aee2251cdda5e841
sachitha2 <shpsachitha@gmail.com>
2020-10-18 23:16:27 +0530

    leave fetch init done

--- src/DAO/LeaveDAO.java
+++ src/DAO/LeaveDAO.java
@@ -1,5 +1,30 @@
 package DAO;
 
-public class LeaveDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class LeaveDAO {
+	Connection connection=null;
+    
+
+	public LeaveDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public ResultSet  EmployeeList() {
+		PreparedStatement ps=null;
+	    String query="SELECT * FROM leave;";
+	    try {
+	        ps=connection.prepareStatement(query);
+	        ResultSet rs=ps.executeQuery();
+	        
+	            return rs;
+	        
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		return null;
+	}
 }
