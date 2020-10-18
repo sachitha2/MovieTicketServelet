933056461b6051ea31d5627d5129398cb4a072dd
sachitha2 <shpsachitha@gmail.com>
2020-10-18 19:38:18 +0530

    server time zone error resolved and jdbc error resolved tom cat lib ekata jdbc jar eka damma ita passe apu error ekata server time zone eka mysql walin haduwa

--- src/Employee/Basic.java
+++ src/Employee/Basic.java
@@ -2,6 +2,10 @@ package Employee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,5 +17,21 @@ public class Basic extends HttpServlet {
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("<h1>Hello World</h1>");
+		
+		DB obj_DB_Connection=new DB();
+        Connection connection=null;
+        connection=obj_DB_Connection.get_connection();
+        PreparedStatement ps=null;
+        String query="SELECT * FROM user;";
+        try {
+            ps=connection.prepareStatement(query);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                out.println(rs.getString("id")+" - "+rs.getString("username"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 	}
 }
---
+++ src/Employee/DB.java
@@ -0,0 +1,23 @@
+package Employee;
+
+
+
+
+import javax.swing.*;
+import java.sql.*;
+
+public class DB {
+
+	
+
+    public Connection get_connection(){
+        Connection connection=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/employeems","root","");
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
