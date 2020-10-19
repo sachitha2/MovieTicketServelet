961846d2d8bd01c4a40ea86873db56cf6768af9f
sachitha2 <shpsachitha@gmail.com>
2020-10-19 11:40:38 +0530

    leave model created

--- src/Model/LeaveModel.java
+++ src/Model/LeaveModel.java
@@ -1,5 +1,59 @@
 package Model;
 
 public class LeaveModel {
-
+	int id;
+	String leaveType;
+	String sDate;
+	String eDate;
+	String reason;
+	int status;
+	public LeaveModel(int id, String leaveType, String sDate, String eDate, String reason, int status) {
+		super();
+		this.id = id;
+		this.leaveType = leaveType;
+		this.sDate = sDate;
+		this.eDate = eDate;
+		this.reason = reason;
+		this.status = status;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLeaveType() {
+		return leaveType;
+	}
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
+	}
+	public String getsDate() {
+		return sDate;
+	}
+	public void setsDate(String sDate) {
+		this.sDate = sDate;
+	}
+	public String geteDate() {
+		return eDate;
+	}
+	public void seteDate(String eDate) {
+		this.eDate = eDate;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+	
 }
+
