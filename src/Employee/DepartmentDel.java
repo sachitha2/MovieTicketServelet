64454a70397bef9bdee7d8b4fa705a3eca1c9914
sachitha2 <shpsachitha@gmail.com>
2020-10-19 13:38:26 +0530

    init all delete jsp files

---
+++ src/Employee/DepartmentDel.java
@@ -0,0 +1,27 @@
+package Employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DepartmentDel")
+public class DepartmentDel extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		//handle delete here
+		if(request.getParameter("id")!=null){
+			out.println("<h1>Hello World</h1>");
+		}else {
+			out.println("Delete id not found");
+		}
+	}
+
+	
+
+}
