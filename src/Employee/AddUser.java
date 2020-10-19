fd6aceaafbd475310104e39127b867135f930fb1
sachitha2 <shpsachitha@gmail.com>
2020-10-19 15:22:11 +0530

    add user post done

---
+++ src/Employee/AddUser.java
@@ -0,0 +1,41 @@
+package Employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AddUser")
+public class AddUser extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DB obj_DB_Connection=new DB();
+		Connection connection=null;
+	    connection=obj_DB_Connection.get_connection();
+		
+		
+		PrintWriter out = response.getWriter();
+		
+		
+		if((request.getParameter("username") != null) &&  (request.getParameter("password") != null)  &&  (request.getParameter("type") != null)) {
+			out.println("parameters ok");
+		}else {
+			out.println("parameters wrong");
+		}
+		doGet(request, response);
+	}
+
+}
