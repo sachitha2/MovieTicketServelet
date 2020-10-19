93f2878e86742218d187d1670bef86224d6de95c
sachitha2 <shpsachitha@gmail.com>
2020-10-19 19:41:28 +0530

    wip

--- src/Employee/UpdateEmp.java
+++ src/Employee/UpdateEmp.java
@@ -1,11 +1,17 @@
 package Employee;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import DAO.EmployeeDAO;
+import Model.EmployeeModel;
 
 @WebServlet("/UpdateEmp")
 public class UpdateEmp extends HttpServlet {
@@ -17,6 +23,27 @@ public class UpdateEmp extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		DB obj_DB_Connection=new DB();
+		Connection connection=null;
+	    connection=obj_DB_Connection.get_connection();
+	    
+	    EmployeeDAO empDAO = new EmployeeDAO(connection);
+		
+		PrintWriter out = response.getWriter();
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		if(request.getParameter("addemp").equals("Submit")) {
+			
+			
+			
+			EmployeeModel empModel = new EmployeeModel(id,request.getParameter("name"),request.getParameter("dob"),request.getParameter("address"),request.getParameter("mobile"),request.getParameter("email"),request.getParameter("nic"),request.getParameter("username"),request.getParameter("pass"));
+			
+			
+			empDAO.updateData(empModel);
+			response.sendRedirect("updateemp.jsp?msg=done&id="+id);
+		}else {
+			response.sendRedirect("updateemp.jsp?msg=failed&id="+id);
+		}
 		doGet(request, response);
 	}
 
