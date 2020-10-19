74b1f15eb789308d4860ae01ca03c464636bbb23
sachitha2 <shpsachitha@gmail.com>
2020-10-19 14:22:11 +0530

    leave del done

--- src/Employee/EmpDel.java
+++ src/Employee/EmpDel.java
@@ -2,6 +2,7 @@ package Employee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,17 +10,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.DepartmentDAO;
+
 @WebServlet("/EmpDel")
 public class EmpDel extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				PrintWriter out = response.getWriter();
-				//handle delete here
-				if(request.getParameter("id")!=null){
-					out.println("<h1>Hello World</h1>");
-				}else {
-					out.println("Delete id not found");
-				}
+		DB obj_DB_Connection=new DB();
+		Connection connection=null;
+	    connection=obj_DB_Connection.get_connection();
+		
+		
+		PrintWriter out = response.getWriter();
+		//handle delete here
+		if(request.getParameter("id")!=null){
+			out.println("<p>Done</p>");
+			DepartmentDAO depDAO = new DepartmentDAO(connection);
+			depDAO.del(request.getParameter("id"));
+		}else {
+			out.println("Delete id not found");
+		}
 	}
 
 	
