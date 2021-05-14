65fedc8e761730843c563381e7eb7cf20760e7c9
Sachitha Hirushan <shpsachitha@gmail.com>
2021-05-14 13:16:20 +0530

    Booking object created

---
+++ src/Model/Booking.java
@@ -0,0 +1,60 @@
+package Model;
+
+public class Booking {
+	int id;
+	int userid;
+	int timeslotId;
+	float total;
+	int numTickets;
+	String date;
+	public Booking(int id, int userid, int timeslotId, float total, int numTickets, String date) {
+		
+		this.id = id;
+		this.userid = userid;
+		this.timeslotId = timeslotId;
+		this.total = total;
+		this.numTickets = numTickets;
+		this.date = date;
+	}
+	public int getId() {
+		return id;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public int getTimeslotId() {
+		return timeslotId;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public int getNumTickets() {
+		return numTickets;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public void setTimeslotId(int timeslotId) {
+		this.timeslotId = timeslotId;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	public void setNumTickets(int numTickets) {
+		this.numTickets = numTickets;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+}
